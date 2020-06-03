ef2ff8d2526d4bd1dd704ea55a0fc398feb099c5
carlos.moura <tre.junior@hotmail.com>
2020-06-02 23:26:33 -0300

    changing readme.md text

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Busquem Conhecimento
         </a>
       </header>
     </div>
